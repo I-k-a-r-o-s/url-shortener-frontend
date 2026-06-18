0fddd8a740f9acb818e222f1cd697f61d6331442
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-18 09:52:14 +0530

    basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="navbar bg-base-200 shadow-sm">
+      <a className="btn btn-ghost text-xl">URL Shortner</a>
+    </div>
+  );
+};
+
+export default Header;
