6c06262ca27d6f83517b7e11c12d130d91946998
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-18 10:36:40 +0530

    proper export

--- src/hooks/useAppContext.ts
+++ src/hooks/useAppContext.ts
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { appContext } from "../context/AppContext";
 
-export const useAppContext = () => {
+const useAppContext = () => {
   const context = useContext(appContext);
   if (!context) {
     throw new Error("useAppContext must be used inside AppContextProvider");
   }
   return context;
 };
+
+export default useAppContext
