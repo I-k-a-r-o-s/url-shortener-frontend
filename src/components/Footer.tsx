1a43edca80486cf83960a95f7f6f83787f05c301
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-18 10:21:02 +0530

    copyright mesage edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,7 @@ const Footer = () => {
     <footer className="footer footer-center border-t border-base-300 bg-base-100 px-4 py-6 text-base-content/70 sm:footer-horizontal">
       <aside>
         <p className="text-center text-sm">
-          Copyright (c) {new Date().getFullYear()} - All rights reserved by ACME
-          Industries Ltd.
+          Copyright (c) {new Date().getFullYear()} - All rights NOT reserved by anyone. Coded for learning purposes.
         </p>
       </aside>
     </footer>
